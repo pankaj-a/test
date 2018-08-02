68b32127efc6c26ecc70a068f48a3b3bb683f428
Pankaj Ahuja <ray.pankaj@gmail.com>
2018-08-02 15:34:02 +0530

    changed config. added secure key

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -1,7 +1,7 @@
 var browserstack = require('browserstack-local');
 
 exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  user: process.env.BROWSERSTACK_USER || 'BROWSERSTACK_USERNAME',
   key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
 
   updateJob: false,
